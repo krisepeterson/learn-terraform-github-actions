2ad0c2943e80650b4cb4d7cace5bb1842d0dda5a
Chris HP <krisepeterson@gmail.com>
2021-09-20 20:48:56 +1000

    Point backend to correct TFC organisation and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "chrispe-labs"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
